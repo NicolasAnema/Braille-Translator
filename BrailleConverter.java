80e73fa537558256e21d5a44c691bda8f57cf9f3
Nicolas <26562014@sun.ac.za>
2024-03-23 14:27:43 +0200

    changes

--- BrailleConverter.java
+++ BrailleConverter.java
@@ -1,114 +1,219 @@
-// import java.io.ObjectOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class provides translation functionality.
+ */
 public class BrailleConverter {
+    /**
+     * The entry point of the program.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        // checking for correct number of args
+        if (args.length != 4) {
+            System.err.println("Input Error - Invalid number of arguments");
+            System.exit(1);
+        }
+            String gui = args[0];
+            String dir = args[1];
+            String level = args[2];
+            String path = args[3];
 
-    // Define a map to store the Braille representation of characters
-    private static final Map<Character, String> charToBrailleMap = new HashMap<>();
-    // Define a map to store the Braille representation of punctuation numbers
-    private static final Map<Character, String> numberToBrailleMap = new HashMap<>();
-    // Define a map to store the Braille representation of punctuation symbols
-    private static final Map<Character, String> punctuationToBrailleMap = new HashMap<>();
-    private static final Map<String, Character> brailleToCharMap = new HashMap<>();
-    private static final Map<String, Character> numberBrailleToCharMap = new HashMap<>();
+            // checking valid GUI option
+            if (!gui.equals("GUI") && !gui.equals("noGUI")) {
+                System.err.println("Input Error - Invalid GUI argument");
+                System.exit(1);
+            }
+            
+            if (!dir.equals("b2t") && !dir.equals("t2b")) {
+                System.err.println("Input Error - Invalid direction");
+                System.exit(1);
+            }
 
-    private static final Map<String, Character> punctuationBrailleToCharMap = new HashMap<>();
+            // checking valid level
+            if (!Arrays.asList("1.0", "1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "2.4", 
+                               "3.1", "3.2", "3.3", "4.1").contains(level)) {
+                System.err.println("Input Error - Invalid level");
+                System.exit(1);
+            }
+
+            String line = null;
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+                line = reader.readLine();
+
+                if (path.endsWith(".brf")) {
+                    if (line == null || !line.matches("[0-9-]+")) {
+                        System.err.println("Input Error - Invalid file content");
+                    }
+                } else if (path.endsWith(".txt")) {
+                    if (line == null || !line.matches(".*[a-zA-Z]+.*")) {
+                        System.err.println("Input Error - Invalid file content");
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Input Error - Invalid or missing file");
+                System.exit(1);
+            }
+
+                if (path.endsWith(".brf") && dir.equals("b2t")) {
+                    try {
+                        String fileName = Paths.get(path).getFileName().toString().
+                        replace(".brf", "");
+                        String newFilePath = "out/" + fileName + "_b2t.txt";
+                        FileWriter fileWriter = new FileWriter(newFilePath);
+                        if (line != null) {
+                            fileWriter.write(brailleToText(line));
+                            fileWriter.write("\n"); // New line
+                        }
+                        fileWriter.close();
+
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while writing to the file.");
+                        e.printStackTrace();
+                    }
+
+                } else if (path.endsWith(".txt")) {
+                    try {
+                        String fileName = Paths.get(path).getFileName().toString().
+                        replace(".txt", "");
+                        String newFilePath = "out/" + fileName + "_t2b.brf";
+                        FileWriter fileWriter = new FileWriter(newFilePath);
+                        if (line != null) {
+                            fileWriter.write(textToBraille(line));
+                            fileWriter.write("\n");
+                        }
+                        fileWriter.close();
+
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while writing to the file.");
+                        e.printStackTrace();
+                    }
+                }
+            
+        }
+
+    // Text to braille maps
+    // Letters
+    private static final Map<Character, String> CHAR_TO_BRAILLE_MAP = new HashMap<>();
+    // Numbers
+    private static final Map<Character, String> NUMBER_TO_BRAILLE_MAP = new HashMap<>();
+    // Punctuations
+    private static final Map<Character, String> PUNCTUATION_TO_BRAILLE_MAP = new HashMap<>();
+
+    // Braille to text maps
+    // Letters
+    private static final Map<String, Character> BRAILLE_TO_CHAR_MAP = new HashMap<>();
+    // Numbers
+    private static final Map<String, Character> NUMBER_BRAILLE_TO_CHAR_MAP = new HashMap<>();
+    // Punctuations
+    private static final Map<String, Character> PUNCTUATION_BRAILLE_TO_CHAR_MAP = new HashMap<>();
 
     // Populate the map with Braille representations
     static {
         // Letters
-        charToBrailleMap.put('a', "1");
-        charToBrailleMap.put('b', "12");
-        charToBrailleMap.put('c', "14");
-        charToBrailleMap.put('d', "145");
-        charToBrailleMap.put('e', "15");
-        charToBrailleMap.put('f', "124");
-        charToBrailleMap.put('g', "1245");
-        charToBrailleMap.put('h', "125");
-        charToBrailleMap.put('i', "24");
-        charToBrailleMap.put('j', "245");
-        charToBrailleMap.put('k', "13");
-        charToBrailleMap.put('l', "123");
-        charToBrailleMap.put('m', "134");
-        charToBrailleMap.put('n', "1345");
-        charToBrailleMap.put('o', "135");
-        charToBrailleMap.put('p', "1234");
-        charToBrailleMap.put('q', "12345");
-        charToBrailleMap.put('r', "1235");
-        charToBrailleMap.put('s', "234");
-        charToBrailleMap.put('t', "2345");
-        charToBrailleMap.put('u', "136");
-        charToBrailleMap.put('v', "1236");
-        charToBrailleMap.put('w', "2456");
-        charToBrailleMap.put('x', "1346");
-        charToBrailleMap.put('y', "13456");
-        charToBrailleMap.put('z', "1356");
+        CHAR_TO_BRAILLE_MAP.put('a', "1");
+        CHAR_TO_BRAILLE_MAP.put('b', "12");
+        CHAR_TO_BRAILLE_MAP.put('c', "14");
+        CHAR_TO_BRAILLE_MAP.put('d', "145");
+        CHAR_TO_BRAILLE_MAP.put('e', "15");
+        CHAR_TO_BRAILLE_MAP.put('f', "124");
+        CHAR_TO_BRAILLE_MAP.put('g', "1245");
+        CHAR_TO_BRAILLE_MAP.put('h', "125");
+        CHAR_TO_BRAILLE_MAP.put('i', "24");
+        CHAR_TO_BRAILLE_MAP.put('j', "245");
+        CHAR_TO_BRAILLE_MAP.put('k', "13");
+        CHAR_TO_BRAILLE_MAP.put('l', "123");
+        CHAR_TO_BRAILLE_MAP.put('m', "134");
+        CHAR_TO_BRAILLE_MAP.put('n', "1345");
+        CHAR_TO_BRAILLE_MAP.put('o', "135");
+        CHAR_TO_BRAILLE_MAP.put('p', "1234");
+        CHAR_TO_BRAILLE_MAP.put('q', "12345");
+        CHAR_TO_BRAILLE_MAP.put('r', "1235");
+        CHAR_TO_BRAILLE_MAP.put('s', "234");
+        CHAR_TO_BRAILLE_MAP.put('t', "2345");
+        CHAR_TO_BRAILLE_MAP.put('u', "136");
+        CHAR_TO_BRAILLE_MAP.put('v', "1236");
+        CHAR_TO_BRAILLE_MAP.put('w', "2456");
+        CHAR_TO_BRAILLE_MAP.put('x', "1346");
+        CHAR_TO_BRAILLE_MAP.put('y', "13456");
+        CHAR_TO_BRAILLE_MAP.put('z', "1356");
 
         // Numbers
-        numberToBrailleMap.put('1', "1");
-        numberToBrailleMap.put('2', "12");
-        numberToBrailleMap.put('3', "14");
-        numberToBrailleMap.put('4', "145");
-        numberToBrailleMap.put('5', "15");
-        numberToBrailleMap.put('6', "124");
-        numberToBrailleMap.put('7', "1245");
-        numberToBrailleMap.put('8', "125");
-        numberToBrailleMap.put('9', "24");
-        numberToBrailleMap.put('0', "245");
+        NUMBER_TO_BRAILLE_MAP.put('1', "1");
+        NUMBER_TO_BRAILLE_MAP.put('2', "12");
+        NUMBER_TO_BRAILLE_MAP.put('3', "14");
+        NUMBER_TO_BRAILLE_MAP.put('4', "145");
+        NUMBER_TO_BRAILLE_MAP.put('5', "15");
+        NUMBER_TO_BRAILLE_MAP.put('6', "124");
+        NUMBER_TO_BRAILLE_MAP.put('7', "1245");
+        NUMBER_TO_BRAILLE_MAP.put('8', "125");
+        NUMBER_TO_BRAILLE_MAP.put('9', "24");
+        NUMBER_TO_BRAILLE_MAP.put('0', "245");
 
         //punctuations
-        punctuationToBrailleMap.put(',', "2");
-        punctuationToBrailleMap.put('.', "256");
-        punctuationToBrailleMap.put(';',"23");
-        punctuationToBrailleMap.put(':',"25");
-        punctuationToBrailleMap.put('=',"6-2356");
-        punctuationToBrailleMap.put('!',"235");
-        punctuationToBrailleMap.put('#',"456-1456");
-        punctuationToBrailleMap.put('/', "456-34");
-        punctuationToBrailleMap.put('?', "236");
-        punctuationToBrailleMap.put('%', "46-356");
-        punctuationToBrailleMap.put('^', "4-26");
-        punctuationToBrailleMap.put('~', "4-35");
-        punctuationToBrailleMap.put('&', "4-12346");
-        punctuationToBrailleMap.put('*', "5-35");
-        punctuationToBrailleMap.put('(', "5-126");
-        punctuationToBrailleMap.put(')', "5-345");
-        // hyphen
-        punctuationToBrailleMap.put('-', "36");
+        PUNCTUATION_TO_BRAILLE_MAP.put(',', "2");
+        PUNCTUATION_TO_BRAILLE_MAP.put('.', "256");
+        PUNCTUATION_TO_BRAILLE_MAP.put(';', "23");
+        PUNCTUATION_TO_BRAILLE_MAP.put(':', "25");
+        PUNCTUATION_TO_BRAILLE_MAP.put('=', "6-2356");
+        PUNCTUATION_TO_BRAILLE_MAP.put('!', "235");
+        PUNCTUATION_TO_BRAILLE_MAP.put('#', "456-1456");
+        PUNCTUATION_TO_BRAILLE_MAP.put('/', "456-34");
+        PUNCTUATION_TO_BRAILLE_MAP.put('?', "236");
+        PUNCTUATION_TO_BRAILLE_MAP.put('%', "46-356");
+        PUNCTUATION_TO_BRAILLE_MAP.put('^', "4-26");
+        PUNCTUATION_TO_BRAILLE_MAP.put('~', "4-35");
+        PUNCTUATION_TO_BRAILLE_MAP.put('&', "4-12346");
+        PUNCTUATION_TO_BRAILLE_MAP.put('*', "5-35");
+        PUNCTUATION_TO_BRAILLE_MAP.put('(', "5-126");
+        PUNCTUATION_TO_BRAILLE_MAP.put(')', "5-345");
 
         // Populate brailleToCharMap
-        for (Map.Entry<Character, String> entry : charToBrailleMap.entrySet()) {
-            brailleToCharMap.put(entry.getValue(), entry.getKey());
+        for (Map.Entry<Character, String> entry : CHAR_TO_BRAILLE_MAP.entrySet()) {
+            BRAILLE_TO_CHAR_MAP.put(entry.getValue(), entry.getKey());
         }
 
-        for (Map.Entry<Character, String> entry : numberToBrailleMap.entrySet()) {
-            numberBrailleToCharMap.put(entry.getValue(), entry.getKey());
+        for (Map.Entry<Character, String> entry : NUMBER_TO_BRAILLE_MAP.entrySet()) {
+            NUMBER_BRAILLE_TO_CHAR_MAP.put(entry.getValue(), entry.getKey());
         }
 
-        for (Map.Entry<Character, String> entry : punctuationToBrailleMap.entrySet()) {
-            punctuationBrailleToCharMap.put(entry.getValue(), entry.getKey());
+        for (Map.Entry<Character, String> entry : PUNCTUATION_TO_BRAILLE_MAP.entrySet()) {
+            PUNCTUATION_BRAILLE_TO_CHAR_MAP.put(entry.getValue(), entry.getKey());
         }
     }
 
-    // Convert text to Braille
+    /**
+     * Converts a given text into Braille representation.
+     *
+     * @param text the text to be converted
+     * @return the Braille representation of the text
+     */
     public static String textToBraille(String text) {
         StringBuilder braille = new StringBuilder();
         String[] words = text.split(" ");
         for (String word : words) {
             boolean isNumber = false;
             boolean isCapitalWord = word.equals(word.toUpperCase());
-            if(isCapitalWord && !word.matches("[0-9]+")){
+            if (isCapitalWord && !word.matches("[0-9]+")) {
                 braille.append("6-6-");
             }
             char[] textChars = word.toCharArray();
             for (char c : textChars) {
-                if(Character.isDigit(c) && !isNumber){
-                    isNumber=true;
+                if (Character.isDigit(c) && !isNumber) {
+                    isNumber = true;
                     braille.append("3456-");
                 }
 
-                if(isNumber && Character.isAlphabetic(c)){
+                if (isNumber && Character.isAlphabetic(c)) {
                     isNumber = false;
                 }
 
@@ -117,13 +222,12 @@ public class BrailleConverter {
                     braille.append("6-");
                 }
                 String brailleChar = null;
-                if(Character.isDigit(c)){
-                    brailleChar = numberToBrailleMap.get(c);
-                }
-                else if (Character.isAlphabetic(c)){
-                    brailleChar = charToBrailleMap.get(Character.toLowerCase(c));
-                }else{
-                    brailleChar = punctuationToBrailleMap.get(Character.toLowerCase(c));
+                if (Character.isDigit(c)) {
+                    brailleChar = NUMBER_TO_BRAILLE_MAP.get(c);
+                } else if (Character.isAlphabetic(c)) {
+                    brailleChar = CHAR_TO_BRAILLE_MAP.get(Character.toLowerCase(c));
+                } else {
+                    brailleChar = PUNCTUATION_TO_BRAILLE_MAP.get(Character.toLowerCase(c));
                     isNumber = false;
                 }
                 braille.append(brailleChar).append("-");
@@ -135,7 +239,12 @@ public class BrailleConverter {
         return braille.toString().trim();
     }
 
-    // Convert Braille to text
+    /**
+     * Converts a given Braille representation into text.
+     *
+     * @param braille the Braille representation to be converted
+     * @return the text representation of the Braille
+     */
     public static String brailleToText(String braille) {
         StringBuilder text = new StringBuilder();
         String[] words = braille.split(" ");
@@ -149,32 +258,32 @@ public class BrailleConverter {
                     isNumber = true;
                     continue;
                 }
-                if("6".equals(brailleChar)){
-                    if(isCapital){
+                if ("6".equals(brailleChar)) {
+                    if (isCapital) {
                         isCapitalWord = true;
                     }
                     isCapital = true;
                     continue;
                 }
 
-                if(isNumber){
-                    Character textChar = numberBrailleToCharMap.get(brailleChar);
-                    if(textChar == null){
-                        textChar = punctuationBrailleToCharMap.get(brailleChar);
+                if (isNumber) {
+                    Character textChar = NUMBER_BRAILLE_TO_CHAR_MAP.get(brailleChar);
+                    if (textChar == null) {
+                        textChar = PUNCTUATION_BRAILLE_TO_CHAR_MAP.get(brailleChar);
                     }
                     text.append(textChar);
-                }else{
+                } else {
                     // Convert each Braille character to text
-                    Character textChar = brailleToCharMap.get(brailleChar);
-                    if(textChar == null){
-                        textChar = punctuationBrailleToCharMap.get(brailleChar);
+                    Character textChar = BRAILLE_TO_CHAR_MAP.get(brailleChar);
+                    if (textChar == null) {
+                        textChar = PUNCTUATION_BRAILLE_TO_CHAR_MAP.get(brailleChar);
                     }
-                    if(isCapital){
+                    if (isCapital) {
                         text.append(Character.toUpperCase(textChar));
-                        if(!isCapitalWord){
-                            isCapital=false;
+                        if (!isCapitalWord) {
+                            isCapital = false;
                         }
-                    }else{
+                    } else {
                         text.append(textChar);
                     }
                 }
@@ -183,14 +292,5 @@ public class BrailleConverter {
             text.append(" ");
         }
         return text.toString().trim();
-    }
-
-    public static void main(String[] args) {
-        // String text = "!";
-        // String braille = textToBraille(text);
-        // System.out.println("Text to Braille: " + braille);
-
-        String convertedText = brailleToText("235");
-        System.out.println("Braille to Text: " + convertedText);
     }
 }
